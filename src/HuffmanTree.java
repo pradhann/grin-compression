35945efb893f92bcde4e7021ddd6490ebecad945
aberthelsen <ashlin.berthelsen@gmail.com>
2017-04-29 13:53:41 -0500

    started on the HuffmanTree class

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -2,11 +2,38 @@ import java.util.Map;
 
 public class HuffmanTree {
 	
+	private static class Node {
+		public int value;
+		public Node left;
+		public Node right;
+		
+		public Node(int value, Node left, Node right) {
+			this.value = value;
+			this.left = left;
+			this.right = right;
+		}
+	}
+	
+	private Node root;
+	
+	
 	public HuffmanTree(Map<Short, Integer> m) {
 		
 	}
 
 	public HuffmanTree(BitInputStream in) {
+		
+	}
+	
+	public void serialize(BitOutputStream out) {
+		
+	}
+	
+	public void encode(BitInputStream in, BitOutputStream out) {
+		
+	}
+	
+	public void decode(BitInputStream in, BitOutputStream out) {
 		
 	}
 }
