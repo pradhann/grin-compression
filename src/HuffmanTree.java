9712da336068524ed02644e8802bf17cdb5f20af
aberthelsen <ashlin.berthelsen@gmail.com>
2017-04-29 21:12:26 -0500

    changes to huffmantree

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -1,30 +1,30 @@
 import java.util.Map;
 
 public class HuffmanTree {
-	
+
 	private static class Node {
 		public int value;
 		public Node left;
 		public Node right;
-		
+
 		public Node(int value, Node left, Node right) {
 			this.value = value;
 			this.left = left;
 			this.right = right;
 		}
 	}
-	
+
 	private Node root;
-	
-	
+
+
 	public HuffmanTree(Map<Short, Integer> m) {
-		
+
 	}
 
 	public HuffmanTree(BitInputStream in) {
 		root = HuffmanTreeHelper(in);
 	}
-	
+
 	public Node HuffmanTreeHelper(BitInputStream in) {
 		Node cur = null;
 		if (in.hasBits()) {
@@ -37,16 +37,34 @@ public class HuffmanTree {
 		}
 		return cur;
 	}
-	
+
 	public void serialize(BitOutputStream out) {
-		
-	}
-	
+		Node cur = this.root;
+		if(cur.value != -1) {
+			out.writeBit(0);
+			out.writeBits(cur.value, 9);
+		} else {
+			out.writeBit(1);
+			serialize
+		}
+		}
+
+
 	public void encode(BitInputStream in, BitOutputStream out) {
-		
+
 	}
-	
+
 	public void decode(BitInputStream in, BitOutputStream out) {
-		
+		while(in.hasBits()) {
+			Node cur = this.root;
+			while(cur.value == -1) {
+				if(in.readBit() == 0) {
+					cur = cur.left;
+				} else {
+					cur = cur.right;
+				}
+			}
+			out.writeBits(cur.value, 9);
+		}
 	}
 }
